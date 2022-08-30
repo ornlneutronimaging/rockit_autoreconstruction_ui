2454ae466eedbfe0fa71214188239cd93c152702
JeanBilheux <bilheuxjm@ornl.gov>
2022-08-30 10:48:45 -0400

    init tables. this refs #15

--- rockit_autoreconstruction_ui/metadata_dialog.py
+++ rockit_autoreconstruction_ui/metadata_dialog.py
@@ -1,11 +1,12 @@
 from qtpy.QtWidgets import QDialog
 from qtpy import QtGui
-
+import json
 import os
 
 from . import load_ui
-from . import refresh_file
-from .utilities.file import read_ascii
+from .utilities.table_handler import TableHandler
+
+LIST_KEYS_TO_IGNORE = ['filename', 'time_stamp', 'time_stamp_user_format']
 
 
 class MetadataDialog(QDialog):
@@ -21,18 +22,33 @@ class MetadataDialog(QDialog):
 									os.path.join('ui',
 												 'dialog_metadata.ui'))
 		self.ui = load_ui(ui_full_path, baseinstance=self)
-		self.setWindowTitle(f"{os.path.basename(self.metadata_file_name)}")
+		self.setWindowTitle(f"Sample, OB and DC metadata file")
+		self.ui.folder_of_input_file.setText(os.path.dirname(self.metadata_file_name))
+		self.ui.name_of_input_file_label.setText(os.path.basename(self.metadata_file_name))
+		self.init_tables()
 
-		# self.ui.folder_label.setText(os.path.dirname(log_file_name))
-		# self.update_display_text()
-		#
-		# if os.path.exists(self.metadata_file_name):
-		# 	self.ui.preview_metadata_pushButton.setVisible(True)
-		# else:
-		# 	self.ui.preview_metadata_pushButton.setVisible(False)
-		#
-		# icon_refresh = QtGui.QIcon(refresh_file)
-		# self.ui.refresh_pushButton.setIcon(icon_refresh)
+		self.load_json()
+
+	def init_tables(self):
+		column_sizes = [100, 300]
+
+		o_sample = TableHandler(table_ui=self.ui.sample_tableWidget)
+		o_sample.set_column_sizes(column_sizes=column_sizes)
+
+		o_ob = TableHandler(table_ui=self.ui.ob_tableWidget)
+		o_ob.set_column_sizes(column_sizes=column_sizes)
+
+		o_dc = TableHandler(table_ui=self.ui.dc_tableWidget)
+		o_dc.set_column_sizes(column_sizes=column_sizes)
+
+	def load_json(self):
+		with open(self.metadata_file_name, 'r') as json_file:
+			data = json.load(json_file)
+
+		# sample
+		sample_dict = data['sample']
+
+
 
 	def ok_pushed(self):
 		self.close()
